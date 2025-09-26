8ffb48241fc927af123fde2a2961d5083d77051c
Tseng Wynn <wynn1212@gmail.com>
2025-09-27 00:28:42 +0800

    Update icon_hash comment description.

--- src/backend.ts
+++ src/backend.ts
@@ -86,7 +86,7 @@ export interface AppOverviewExt extends AppOverview {
   sort_as: string; // base
   icon_data: string; // base, base64 encoded image
   icon_data_format: string; // base, image type without "image/" (e.g.: jpg, png)
-  icon_hash: string; // base, url hash to fetch the icon for steam games (e.g.: "/assets/" + appid + "_icon.jpg?v=" + icon_hash)
+  icon_hash: string; // base, url hash to fetch the icon for steam games (e.g.: "/assets/" + appid + "/" + icon_hash + ".jpg")
   m_gameid: string; // base, id for non-steam games
 }
 
